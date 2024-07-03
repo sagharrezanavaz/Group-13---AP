01965eb465dbad70d205161313ad1a0e6fb34e2d
mahsasarsangi9006 <mahsa.sarsangi@gmail.com>
2024-07-03 13:08:35 +0330

    add

--- coffeeshop/store/urls.py
+++ coffeeshop/store/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('store-management/', views.store_management, name='store-management'),
     path('add-to-cart/', views.add_to_cart, name="add-to-cart"),
     path('remove-cart/<int:cart_id>/', views.remove_cart, name="remove-cart"),
+    path('remove-cart/<int:cart_id>/', views.remove_cart, name="remove-cart"),
     path('plus-cart/<int:cart_id>/', views.plus_cart, name="plus-cart"),
     path('minus-cart/<int:cart_id>/', views.minus_cart, name="minus-cart"),
     path('cart/', views.cart, name="cart"),
